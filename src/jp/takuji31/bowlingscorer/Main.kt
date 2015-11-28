f1bddd1493fc715b8a8473ac7a0fa91806800ba5
Takuji Nishibayashi <takuji31@gmail.com>
2015-11-28 22:16:56 +0900

    add score

--- src/jp/takuji31/bowlingscorer/Main.kt
+++ src/jp/takuji31/bowlingscorer/Main.kt
@@ -31,6 +31,55 @@ class Main {
                     Frame.Spare(Score.NormalScore(9)),
                     Frame.TenFrame(Score.NormalScore(9), Score.Spare(1), Score.NormalScore(9))
             )))}")
+
+            println("1st : ${Frame.calculate(Frame.from(arrayListOf(
+                    Frame.NormalFrame(Score.NormalScore(2), Score.NormalScore(6)),
+                    Frame.NormalFrame(Score.NormalScore(8), Score.NormalScore(1)),
+                    Frame.NormalFrame(Score.NormalScore(4), Score.Miss),
+                    Frame.NormalFrame(Score.NormalScore(6), Score.NormalScore(3)),
+                    Frame.NormalFrame(Score.Split(8), Score.Miss),
+                    Frame.NormalFrame(Score.NormalScore(2), Score.NormalScore(3)),
+                    Frame.NormalFrame(Score.NormalScore(3), Score.NormalScore(4)),
+                    Frame.NormalFrame(Score.NormalScore(6), Score.NormalScore(3)),
+                    Frame.NormalFrame(Score.Split(8), Score.Miss),
+                    Frame.TenFrame(Score.NormalScore(7), Score.NormalScore(1))
+            )))}")
+            println("2nd : ${Frame.calculate(Frame.from(arrayListOf(
+                    Frame.Strike(),
+                    Frame.Spare(Score.NormalScore(9)),
+                    Frame.NormalFrame(Score.NormalScore(5), Score.Miss),
+                    Frame.NormalFrame(Score.NormalScore(6), Score.NormalScore(3)),
+                    Frame.NormalFrame(Score.NormalScore(9), Score.Miss),
+                    Frame.Spare(Score.NormalScore(8)),
+                    Frame.Spare(Score.Split(6)),
+                    Frame.NormalFrame(Score.NormalScore(9), Score.Miss),
+                    Frame.NormalFrame(Score.NormalScore(6), Score.NormalScore(3)),
+                    Frame.TenFrame(Score.NormalScore(7), Score.NormalScore(2))
+            )))}")
+            println("3rd : ${Frame.calculate(Frame.from(arrayListOf(
+                    Frame.NormalFrame(Score.NormalScore(1), Score.NormalScore(8)),
+                    Frame.Spare(Score.NormalScore(7)),
+                    Frame.Strike(),
+                    Frame.Strike(),
+                    Frame.Spare(Score.NormalScore(8)),
+                    Frame.Spare(Score.NormalScore(8)),
+                    Frame.NormalFrame(Score.NormalScore(5), Score.NormalScore(3)),
+                    Frame.Spare(Score.NormalScore(8)),
+                    Frame.Spare(Score.NormalScore(5)),
+                    Frame.TenFrame(Score.NormalScore(7), Score.NormalScore(2))
+            )))}")
+            println("4th : ${Frame.calculate(Frame.from(arrayListOf(
+                    Frame.NormalFrame(Score.NormalScore(9), Score.Miss),
+                    Frame.NormalFrame(Score.NormalScore(5), Score.Miss),
+                    Frame.NormalFrame(Score.NormalScore(7), Score.Miss),
+                    Frame.Spare(Score.NormalScore(5)),
+                    Frame.NormalFrame(Score.NormalScore(8), Score.NormalScore(1)),
+                    Frame.NormalFrame(Score.NormalScore(6), Score.NormalScore(2)),
+                    Frame.Spare(Score.NormalScore(6)),
+                    Frame.NormalFrame(Score.NormalScore(6), Score.NormalScore(2)),
+                    Frame.Strike(),
+                    Frame.TenFrame(Score.NormalScore(9), Score.Spare(1), Score.NormalScore(8))
+            )))}")
         }
     }
 }
